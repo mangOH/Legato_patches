39e4d650552d19925f7533071dc4d0919ff7cd93
David Frey <dfrey@sierrawireless.com>
2018-02-08 13:42:40 -0800

    Make Legato version directories instead of branches
    The new layout should make the repository easier to use and maintain.
    The apply_legato_patches.sh script now takes an additional argument
    which is the tag to apply the patches onto.

--- apply_legato_patches.sh
+++ apply_legato_patches.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-if [ $# -ne 1 ]
+if [ $# -ne 2 ]
 then
-    echo "Usage: $0 /path/to/Legato/root/directory"
+    echo "Usage: $0 /path/to/Legato/root/directory release_tag"
     exit 1
 fi
 
 LEGATO_ROOT=$1
+PATCH_BASE=$2
 
 if ! [ -d $LEGATO_ROOT ]
 then
@@ -14,15 +15,33 @@ then
     exit 1
 fi
 
+(cd $LEGATO_ROOT && git rev-parse $PATCH_BASE)
+if [ $? -ne 0 ]
+then
+    echo "\"$PATCH_BASE\" doesn't appear to be a valid release tag"
+    exit 1
+fi
+
+if ! [ -d $PATCH_BASE ]
+then
+    echo "No patch directory exists for \"$PATCH_BASE\""
+    exit 1
+fi
+
 NOW=`date +"%Y-%m-%d_%H_%M_%S"`
 PATCH_BRANCH="mangOH_patches_$NOW"
-PATCH_BASE="18.01.0"
 
 apply_patchset() {
-    PATCH_SRC_DIR=`readlink -f $1`
+    PATCH_SRC_DIR=$PATCH_BASE/$1
     PATCH_DEST_DIR=$2
+    PATCH_SRC_DIR_ABS=`readlink -f $PATCH_SRC_DIR`
+    if ! [ -d $PATCH_SRC_DIR_ABS ]
+    then
+        return
+    fi
+
     echo "==========================="
-    echo "Applying patches from $PATCH_SRC_DIR to $PATCH_DEST_DIR on branch $PATCH_BRANCH"
+    echo "Applying patches from $PATCH_SRC_DIR_ABS to $PATCH_DEST_DIR on branch $PATCH_BRANCH"
     echo "==========================="
     (cd $PATCH_DEST_DIR && git checkout -b $PATCH_BRANCH $PATCH_BASE)
     if [ $? -ne 0 ]
@@ -30,7 +49,7 @@ apply_patchset() {
         echo "Couldn't create branch in $PATCH_DEST_DIR"
         exit $?
     fi
-    (cd $PATCH_DEST_DIR && git am $PATCH_SRC_DIR/*.patch)
+    (cd $PATCH_DEST_DIR && git am $PATCH_SRC_DIR_ABS/*.patch)
     if [ $? -ne 0 ]
     then
         echo "Command failed with exit code: $?"
@@ -41,6 +60,9 @@ apply_patchset() {
 
 apply_patchset legato       $LEGATO_ROOT
 apply_patchset wifi         $LEGATO_ROOT/modules/WiFi
+apply_patchset av_connector $LEGATO_ROOT/apps/platformServices/airVantageConnector
+apply_patchset lwm2mcore    $LEGATO_ROOT/3rdParty/Lwm2mCore
+apply_patchset wakaama      $LEGATO_ROOT/3rdParty/Lwm2mCore/wakaama
 
 echo "==========================="
 echo "All patches applied successfully"
